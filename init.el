5c853363af8a3462b3e8df35af6e2257c4921e13
JDFight <jdfight@gmail.com>
2017-01-07 16:39:51 -0500

    Fixed init.el error

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
 ;; multiple-cursors
 (require 'multiple-cursors)
 
-(global-set-key (kbd "C-c m c")) 'mc/edit-lines
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
